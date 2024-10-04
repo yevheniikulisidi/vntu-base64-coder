fa3040af603c27c13accf21e1f33f332cac51849
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2024-10-04 22:13:09 +0300

    Small improvement of copy button

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ class App {
   fallbackCopyTextToClipboard(text) {
     const textArea = document.createElement('textarea');
     textArea.value = text;
-    textArea.style.position = 'fixed'; // Avoid scrolling to bottom
+    textArea.style.position = 'fixed';
     textArea.style.opacity = '0';
     document.body.appendChild(textArea);
     textArea.focus();
